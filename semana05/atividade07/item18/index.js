849ca062c5af2b54b92bf9c53dca5b2bf0a402aa
Matheus Moreira <mthsmoreira59@gmail.com>
2023-01-28 14:27:48 -0300

    atualizando item 18

--- semana05/atividade07/item18/index.js
+++ semana05/atividade07/item18/index.js
@@ -1,25 +1,24 @@
-let preco = 100;
-let parcela = 11;
+let preco = 1000;
+let parcelas = 10;
+let precoParcelado =  preco / parcelas;
+let juros = 0;
 
-
-if(parcela == 1) {
-    let desconto = 0.25 * preco;
+if(parcelas == 1) {
+    const desconto = 0.25 * preco;
     preco = preco - desconto;
     console.log(`Você pagou à vista, ganhou um desconto de R$${desconto}. Preço total: ${preco}`);
-} else if (parcela >= 2 && parcela <= 5) {
-    console.log(`Você parcelou ${parcela}x. Preço total: ${preco}`);
-} else if (parcela >= 6 && parcela <= 10){
-    for(i = 1; i < parcela; i++) {
-        preco =  preco + (0.06 * parcela);
-    }
-    console.log(`Você parcelou ${parcela}x com juros de 13% ao mês. Preço total: ${preco}`);
-} else {
-    for(i = 1; parcela > i; i++) {
-        preco =  preco + (0.13 * parcela);
-    }
-    console.log(`Você parcelou ${parcela}x com juros de 6% ao mês. Preço total: ${preco}`);
-}     
+} else if (parcelas >= 2 && parcelas <= 5) {
+    console.log(`Você parcelou ${parcelas}x de ${precoParcelado.toFixed(2)} . Preço total: ${preco}`);
+} else if (parcelas >= 6 && parcelas <= 10){
+    juros = preco * 0.06 * parcelas;
+    preco = preco + juros;
+    precoParcelado = preco / parcelas;
 
-
-
-
+    console.log(`Você parcelou ${parcelas}x de ${precoParcelado.toFixed(2)} com juros de 6% ao mês. Preço total: ${preco}`);
+} else { 
+    juros = preco * 0.13 * parcelas;
+    preco = preco + juros;
+    precoParcelado = preco / parcelas;
+    
+    console.log(`Você parcelou ${parcelas}x de ${precoParcelado.toFixed(2)} com juros de 13% ao mês. Preço total: ${preco}`);
+}
